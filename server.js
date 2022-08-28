045f2a2837eb773de4725af2fdeb86cba13b3ce2
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-08-28 19:10:05 -0400

    Create a homepage  -Users can view all blog posts in a list  -USers can see how many comments each blog has

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const routes = require("./controllers");
+
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
-    console.log(`App listening on port http://localhost:${PORT}!`)
+    console.log(`App listening on port http://localhost:${PORT}`)
   );
 });
